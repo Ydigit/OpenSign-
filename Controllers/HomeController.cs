b2c173e501feb6c7f455bda061f1c91f637bbf20
Dinis Ramos <119667762+DinisJR2004@users.noreply.github.com>
2025-04-25 14:48:06 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,29 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using OpenSign_.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace OpenSign_.Controllers;
-
-public class HomeController : Controller
+namespace OpenSign.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        public IActionResult Index()
+        {
+            //TempData["Success"] = "Sucesso";
+            //TempData["Error"] = "Erro";
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Help()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Error()
+        {
+            return View();
+        }
     }
 }
+
+
