c4437201410a4130baa9161d8da903e3bb2a5a79
Ricardo-andradee <ricardovenancio2004@gmail.com>
2025-05-12 02:14:22 +0100

    Import and Signature buttons done | Signature page created

---
+++ Controllers/SignatureController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenSign.Controllers
+{
+    [Route("Signature")]//esta e a noma rota base do controllador
+    public class SignatureController : Controller
+    {
+        //QUANDO ACEDER AO ROUTE SIGN, VAI ACEDER AO SIGNVIEW E RETORNA 
+        [HttpGet("")]// vai definir que a action SignView ser� chamada quando a URL /Sign for acessada.
+        public IActionResult Signature()
+        {
+            return View();
+        }
+    }
+}
