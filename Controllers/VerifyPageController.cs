71c0ba02c6e2e853bc63c2a071e41eac75c18bd5
Ricardo-andradee <ricardovenancio2004@gmail.com>
2025-05-22 20:20:56 +0100

    NewPages

--- Controllers/VerifyPageController.cs
+++ Controllers/VerifyPageController.cs
@@ -3,12 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace OpenSign.Controllers
 {
-    [Route("Verify")]//esta e a noma rota base do controllador
+    [Route("VerifyPage")]//esta e a noma rota base do controllador
     public class VerifyPageController : Controller
     {
         //QUANDO ACEDER AO ROUTE SIGN, VAI ACEDER AO SIGNVIEW E RETORNA 
         [HttpGet("")]// vai definir que a action SignView ser� chamada quando a URL /Sign for acessada.
-        public IActionResult VerifyPageView()
+        public IActionResult VerifyPage()
         {
             return View();
         }
